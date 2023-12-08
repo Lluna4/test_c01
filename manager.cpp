42b0231e468dddb9d6be163c66dbeed7cea85e55
Luna <holacarly500@gmail.com>
2023-12-08 20:13:38 +0100

    Add directory change and include headers

--- manager.cpp
+++ manager.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <libgen.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     int status = 0;
+    char *dir = dirname(argv[0]);
+    chdir(dir);
     system("ls");
     status = system("g++ -std=c++17 test.cpp -o test");
     if (status == 0)
