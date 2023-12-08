dbdec3b13feef26c7ac673e9155c43e4371927a6
Luna <holacarly500@gmail.com>
2023-12-08 20:20:26 +0100

    a

--- manager.cpp
+++ manager.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <libgen.h>
 #include <unistd.h>
+#include <format>
 
 int main(int argc, char *argv[])
 {
     int status = 0;
     char *dir = dirname(argv[0]);
-    chdir(dir);
-    system("ls");
+    system(std::format("cp {}/test.cpp ./test.cpp", dir).c_str());
     status = system("g++ -std=c++17 test.cpp -o test");
     if (status == 0)
     {
