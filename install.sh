ca1965aa2c40ed29babb564fd8da60b255fc333e
Luna <69774917+Lluna4@users.noreply.github.com>
2023-12-09 12:33:52 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ REPO_URL="https://github.com/Lluna4/test_c01"
 git clone $REPO_URL test_c01
 cd test_c01
 
-g++ manager.cpp -std=c++17 -o test_c01
+g++ manager.cpp -std=c++17
 
 path="$(cd "$(dirname "$0")"; pwd -P)"
 if [ -z "$path" ]; then
