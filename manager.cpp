498cd062641b41e27d34dadcdf744ff094dd418e
Luna <69774917+Lluna4@users.noreply.github.com>
2023-12-09 11:25:50 +0100

    Add files via upload

--- manager.cpp
+++ manager.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <libgen.h>
 #include <unistd.h>
-#include <format>
 
 int main(int argc, char *argv[])
 {
     int status = 0;
     char *dir = dirname(argv[0]);
-    system(std::format("cp {}/test.cpp ./test.cpp", dir).c_str());
+    char *buf = (char *)malloc(1024 * sizeof(char));
+    sprintf(buf, "cp %s/test.cpp ./test.cpp", dir);
+    system(buf);
+    free(buf);
     status = system("g++ -std=c++17 test.cpp -o test");
     if (status == 0)
     {
